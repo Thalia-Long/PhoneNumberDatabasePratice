c8cd22a1ab9e9da85eac8e1f431506574ce69b78
Hien <Hien@Hienlong.fios-router.home>
2017-05-10 15:35:58 -0400

    Starting the project

---
+++ src/phonenumberdatabasepractice/PhoneNumberDatabasePractice.java
@@ -0,0 +1,33 @@
+/*
+Write a java program that loads names and phone numbers from a database table 
+where It takes a name or phone number as input and prints the corresponding other
+value from the database table. (Hint: use hash tables to load the data from the database table).
+================================================================================
+Sample Output:
+
+Table with name and mobile number,
+When you run the code, there should be a menu
+1. Search by name
+2. Search by mobile number
+3. Exit
+Enter your option 1, 2, 3   [if you enter 1]
+Enter the name,
+XXXX [search the data, which is already loaded into hashtable and return a message
+that its available or not]
+Enter option 1, 2, 3 [ if you enter 2]
+Enter the mobile number
+9999[search the data, which is already loaded into hashtable and return a message]
+================================================================================
+
+Write a java program that takes tab separated data (one record per line) from a 
+text file and inserts them into database
+ */
+package phonenumberdatabasepractice;
+
+/**
+ *
+ * @author Hien Long
+ */
+public class PhoneNumberDatabasePractice {
+    
+}
---
+++ src/phonenumberdatabasepractice/PhoneNumberDatabasePractice_Test.java
@@ -0,0 +1,22 @@
+/*
+    Test class
+ */
+package phonenumberdatabasepractice;
+
+/**
+ *
+ * @author Hien Long
+ */
+public class PhoneNumberDatabasePractice_Test {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      
+        
+        
+        
+    }
+    
+}
