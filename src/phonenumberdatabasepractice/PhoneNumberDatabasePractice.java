46bcff71018353ea8c6bd83bcb50fbd6e7d7f66e
Hien <Hien@Hienlong.fios-router.home>
2017-05-12 15:55:24 -0400

    create methods that need to be implemented

--- src/phonenumberdatabasepractice/PhoneNumberDatabasePractice.java
+++ src/phonenumberdatabasepractice/PhoneNumberDatabasePractice.java
@@ -1,4 +1,4 @@
-/*
+/*  DAY 11
 Write a java program that loads names and phone numbers from a database table 
 where It takes a name or phone number as input and prints the corresponding other
 value from the database table. (Hint: use hash tables to load the data from the database table).
@@ -24,10 +24,48 @@ text file and inserts them into database
  */
 package phonenumberdatabasepractice;
 
+import java.sql.Connection;
+
 /**
  *
  * @author Hien Long
  */
 public class PhoneNumberDatabasePractice {
+    /*
+    class field declarations
+    */
+    private Connection connection;
+    /*
+    Constructor who takes the String needed to connect to a database
+    */
+    public PhoneNumberDatabasePractice( ){
+        
+    }
+    
+    /*
+    Connect to a database
+    */
+    
+    public Connection getConnection(){
+        return connection;
+    }
+    
+    /*
+    Get name corresponed to the phone number input
+    */
+    public String getName(){
+        String name = null;
+        return name;
+    }
+    
+    /*
+    Get phone number corresponded to the name input
+    */
+    public String getPhone(){
+        String phone = null;
+        return phone;
+    }
+    
+    
     
 }
