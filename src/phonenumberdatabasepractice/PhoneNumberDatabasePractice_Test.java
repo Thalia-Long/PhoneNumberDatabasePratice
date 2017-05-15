1b2319f59410f2b2d361afe6bf825bebb2ca1395
Hien <Hien@Hienlong.fios-router.home>
2017-05-15 11:16:53 -0400

    display message to user

--- src/phonenumberdatabasepractice/PhoneNumberDatabasePractice_Test.java
+++ src/phonenumberdatabasepractice/PhoneNumberDatabasePractice_Test.java
@@ -3,20 +3,41 @@
  */
 package phonenumberdatabasepractice;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Hien Long
  */
 public class PhoneNumberDatabasePractice_Test {
 
+    /*
+    Declare final String for database connection
+     */
+
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-      
-        
-        
-        
+
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("1. Search by name\n"
+                + "2. Search by mobile number\n"
+                + "3. Exit\n"
+                + "Enter your option 1, 2, 3");
+        String input;
+        input = keyboard.nextLine();
+        switch (input) {
+            case "1":
+                System.out.println("Please enter a name to search: ");
+                input = keyboard.nextLine();
+
+            case "2":
+                System.out.println("Please enter a phone number to search: ");
+                input = keyboard.nextLine();
+            case "3":
+                System.exit(0);
+        }
     }
-    
+
 }
