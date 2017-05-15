701afce98d07b603a51801524fe2c11fac426bfe
Hien Long <>
2017-05-15 11:48:14 -0400

    Rough finished version for now. Come back and compile it with the real JDBC connection datas

--- src/phonenumberdatabasepractice/PhoneNumberDatabasePractice_Test.java
+++ src/phonenumberdatabasepractice/PhoneNumberDatabasePractice_Test.java
@@ -14,12 +14,16 @@ public class PhoneNumberDatabasePractice_Test {
     /*
     Declare final String for database connection
      */
+    static final String JDBC_DRIVER = "com.mysqlmjdbc.Driver";
+    static final String DB_URL = "jdbc:mysql://localhost/EMPLOYEES";
+    static final String USER = "username";
+    static final String PASS = "password";
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-
+        PhoneNumberDatabasePractice databasePractice = new PhoneNumberDatabasePractice(JDBC_DRIVER, DB_URL, USER, PASS);
         Scanner keyboard = new Scanner(System.in);
         System.out.println("1. Search by name\n"
                 + "2. Search by mobile number\n"
@@ -31,10 +35,11 @@ public class PhoneNumberDatabasePractice_Test {
             case "1":
                 System.out.println("Please enter a name to search: ");
                 input = keyboard.nextLine();
-
+                databasePractice.getPhone(input);
             case "2":
                 System.out.println("Please enter a phone number to search: ");
                 input = keyboard.nextLine();
+                databasePractice.getName(input);
             case "3":
                 System.exit(0);
         }
